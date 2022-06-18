f05ec98727e66e9c0ed28556bc80e6edada89d7f
nigelndlovu <ndl20004@byui.edu>
2022-06-18 10:46:33 +0200

    home page continued

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -5,42 +5,41 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
-let date = new Date();
-let year = date.getFullYear();
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+    "Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+const date = new Date();
+const dayName = daynames[date.getDay()];
+const monthName = months[date.getMonth()];
+const year = date.getFullYear();
+const fulldate = `${dayName}, ${date.getDate()} ${monthName} ${year}`;
+document.querySelector(".headerdate").innerHTML= fulldate;
 document.querySelector('#getyear').textContent = year;
-
 document.querySelector('#lastmod').textContent = document.lastModified;
-
-//const daynames = [
-//	"Sunday",
-//	"Monday",
-//	"Tuesday",
-//	"Wednesday",
-//	"Thursday",
-//	"Friday",
-//	"Saturday"
-//];
-//const months = [
-//	"January",
-//	"February",
-//	"March",
-//	"April",
-//	"May",
-//	"June",
-//	"July",
-//	"August",
-//	"September",
-//	"October",
-//	"November",
-//	"December"
-//];
-//const d = new Date();
-//const dayName = daynames[d.getDay()];
-//const monthName = months[d.getMonth()];
-//const year = d.getFullYear();
-//document.querySelector('#getyear').textContent = year;
-//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-//const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-//document.getElementById("todaydate").textContent = fulldate;
-//document.getElementById("currentdate").textContent = fulldate;
-// *****************************
+const bannerdate = document.querySelector(".banner");
+if( date.getDay() === 1 || date.getDay() === 2) {
+    bannerdate.style.display = "flex";
+} else {
+    bannerdate.style.display = "none";
+}
