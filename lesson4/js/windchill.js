608b8e2b8908aeb44cf75bb6393676f08820401c
nigelndlovu <ndl20004@byui.edu>
2022-07-20 06:00:41 +0200

    commit

--- lesson4/js/windchill.js
+++ lesson4/js/windchill.js
@@ -4,13 +4,13 @@ let s = parseFloat(document.getElementById("speed").innerHTML);
 
 let result = windChill(t, s);
 
-if (t<=10 && s>4.8) {
-    document.getElementById("chill").innerHTML = result.toFixed(2);
+if (t<=10.0 && s>4.8) {
+	document.getElementById("chill").innerHTML = result.toFixed(2);
 } else {
-    document.getElementById("chill").innerHTML = "N/A";
+  document.getElementById("chill").innerHTML = "N/A";
 }
 
 function windChill(t, s) {
-    let f = 13.12 + (0.6125 * t) - (11.37 * (s**0.16)) + (0.3965 * t * (s**0.16));
-    return f;
+  let f = 13.12 + (0.6125 * t) - (11.37 * (s**0.16)) + (0.3965 * t * (s**0.16));
+  return f;
 }
